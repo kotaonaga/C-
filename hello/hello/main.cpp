9710562e70f33108494b6e400ad71400a0df2fda
Kota Onaga <kohtaonaga@gmail.com>
2020-05-22 23:33:35 +0900

    added hello

---
+++ hello/hello/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by kota on 2020/05/22.
+//  Copyright © 2020 kota. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!!\n";
+    return 0;
+}
