7d9c22230761af2560e7f8acdda826abbd99a4ef
Kota Onaga <kohtaonaga@gmail.com>
2020-06-10 23:40:45 +0900

    mapとcallingStudyを追加

---
+++ map/map/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  map
+//
+//  Created by kota on 2020/06/10.
+//  Copyright © 2020 kota. All rights reserved.
+//
+
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+    map<string, int> mapTest;
+    mapTest["apple"] = 3;
+    mapTest["orange"] = 4;
+}
